533ff494661c0f4c460d197cb9eb6b0be3d475a7
Matt Davis <matt_davis@me.com>
2017-04-12 19:28:07 -0500

    db, controller, setup ready for test

--- controllers/bowling_api.js
+++ controllers/bowling_api.js
@@ -59,25 +59,25 @@ router.get('/:name', (req, res) => {
  * Outputs player object as JSON
  */
 router.put('/:name', (req, res) => {
-  const name     = req.params.name,
-        currRoll = req.body.currRoll;
+  const name = req.params.name,
+        roll = req.body.roll;
 
   if (typeof name === 'undefined' || name === '') {
     res.status(400).send('Name parameter required');
 
-  } else if (typeof currRoll !== 'number' || currRoll < 0 || currRoll > 10) {
-    res.status(400).send('Last roll must be a number between 0 and 10');
-
-  } else {
-    const player = db.get(name);
-
-    if (bowl.validateRoll(player, currRoll))
-
-    // Not required here since we're mutating the object, but would be in a normal db
-    // db.set(name, player);
-
-    res.status(400).send('Name parameter required');
+  } else if (typeof roll !== 'number' || roll < 0 || roll > 10) {
+    res.status(400).send('Roll must be a number between 0 and 10');
   }
+
+  const player = db.get(name);
+
+  if (!bowl.validateRoll(player, roll)) {
+    res.status(400).send('Invalid roll: open frame must not exceed 10 total');
+  }
+
+  bowl.updatePlayer(player, roll);
+  db.update(name, player);
+  res.json(player);
 });
 
 
