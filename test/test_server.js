83f53f5374b85e94d27359f4604f2b9a1bec826b
Matt Davis <matt_davis@me.com>
2017-04-13 00:09:04 -0500

    reverted to Mocha callbacks

--- test/test_server.js
+++ test/test_server.js
@@ -9,12 +9,12 @@ const chai     = require('chai'),
 
 chai.use(chaiHTTP);
 
-describe('Bowling API: REST routes', () => {
-  it('should add a SINGLE player on /api POST', () => {
+describe('Bowling API: Basic routes', () => {
+  it('should add a SINGLE player on /api POST', done => {
     chai.request(server)
       .post('/api')
       .send({ name: 'Jane Doe' })
-      .then(res => {
+      .end((err, res) => {
         res.should.have.status(201);
         res.body.should.be.a('object');
         res.body.should.have.property('id');
@@ -23,143 +23,109 @@ describe('Bowling API: REST routes', () => {
         res.body.should.have.property('frames').with.length(0);
         res.body.should.have.property('onFrame').eql(1);
         res.body.should.have.property('gameOver').eql(false);
-      })
-      .catch(err => { throw err });
+        done();
+      });
   });
   
-  it('should list ALL players on /api GET', () => {
+  it('should list ALL players on /api GET', done => {
     chai.request(server)
       .get('/api')
-      .then(res => {
+      .end((err, res) => {
         res.should.have.status(200);
         res.body.should.be.a('array');
         res.body.length.should.be.eql(1);
-      })
-      .catch(err => { throw err });;
+        done();
+      });
   });
   
-  it('should list a SINGLE player on /api/<name> GET', () => {
+  it('should list a SINGLE player on /api/<name> GET', done => {
     chai.request(server)
       .get('/api/Jane%20Doe')
-      .then(res => {
+      .end((err, res) => {
         res.should.have.status(200);
         res.body.should.be.a('object');
-      })
-      .catch(err => { throw err });
+        done();
+      });
   });
 
-  it('should update a SINGLE player on /api/<name> PUT', () => {
+  it('should update a SINGLE player on /api/<name> PUT', done => {
     chai.request(server)
       .put('/api/Jane%20Doe')
       .send({ roll: 10 })
-      .then(res => {
+      .end((err, res) => {
         res.should.have.status(200);
         res.body.should.be.a('object');
         res.body.should.have.property('score').eql(10);
         res.body.should.have.property('frames').with.length(1);
         res.body.should.have.property('onFrame').eql(2);
         res.body.should.have.property('gameOver').eql(false);
-      })
-      .catch(err => { throw err });
+        done();
+      });
   });
 
-  it('should delete a SINGLE player on /api/<name> DELETE', () => {
+  it('should delete a SINGLE player on /api/<name> DELETE', done => {
     chai.request(server)
       .delete('/api/Jane%20Doe')
-      .then(res => {
+      .end((err, res) => {
         res.should.have.status(204);
 
         chai.request(server)
           .get('/api/Jane%20Doe')
-          .then(res => {
+          .end((err, res) => {
             res.should.have.status(200);
             res.body.should.be.a('string');
-          })
-          .catch(err => { throw err });;
-      })
-      .catch(err => { throw err });
+            done();
+          });
+      });
   });
 });
 
 describe('Bowling API: Scoring', () => {
-  // beforeEach((done) => {
-  //   chai.request(server)
-  //     .post('/api')
-  //     .send({ name: 'Jane Doe' })
-  //     .end((err, res) => {
-  //       if (err) console.error(err);
-  //       done();
-  //     })
-  // });
+  beforeEach((done) => {
+    chai.request(server)
+      .post('/api')
+      .send({ name: 'Jane Doe' })
+      .end((err, res) => {
+        res.should.have.status(201);
+        res.body.should.be.a('object');
+        res.body.should.have.property('id');
+        res.body.should.have.property('name');
+        res.body.should.have.property('score').eql(0);
+        res.body.should.have.property('frames').with.length(0);
+        res.body.should.have.property('onFrame').eql(1);
+        res.body.should.have.property('gameOver').eql(false);
+        done();
+      });    
+  });
 
-  // afterEach((done) => {
-  //   chai.request(server)
-  //     .delete('/api/Jane%20Doe')
-  //     .end((err, res) => {
-  //       if (err) console.error(err);
-  //       done();
-  //     })
-  // });
+  afterEach((done) => {
+    chai.request(server)
+      .delete('/api/Jane%20Doe')
+      .end((err, res) => {
+        res.should.have.status(204);
+
+        chai.request(server)
+          .get('/api/Jane%20Doe')
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('string');
+            done();
+          });
+      });
+  });
   
-  it('should have score 0 if all rolls are 0', () => {
-    chai.request(server).post('/api').send({ name: 'Jane Doe' })
-      .then(() => chai.request(server).put('/api/Jane%20Doe').send({ roll: 0 }))
-      .then(() => chai.request(server).put('/api/Jane%20Doe').send({ roll: 0 }))
-      .then(() => chai.request(server).put('/api/Jane%20Doe').send({ roll: 0 }))
-      .then(() => chai.request(server).put('/api/Jane%20Doe').send({ roll: 0 }))
-      .then(() => chai.request(server).put('/api/Jane%20Doe').send({ roll: 0 }))
-      .then(() => chai.request(server).put('/api/Jane%20Doe').send({ roll: 0 }))
-      .then(() => chai.request(server).put('/api/Jane%20Doe').send({ roll: 0 }))
-      .then(() => chai.request(server).put('/api/Jane%20Doe').send({ roll: 0 }))
-      .then(() => chai.request(server).put('/api/Jane%20Doe').send({ roll: 0 }))
-      .then(() => chai.request(server).put('/api/Jane%20Doe').send({ roll: 0 }))
-      .then(() => chai.request(server).put('/api/Jane%20Doe').send({ roll: 0 }))
-      .then(() => chai.request(server).put('/api/Jane%20Doe').send({ roll: 0 }))
-      .then(() => chai.request(server).put('/api/Jane%20Doe').send({ roll: 0 }))
-      .then(() => chai.request(server).put('/api/Jane%20Doe').send({ roll: 0 }))
-      .then(() => chai.request(server).put('/api/Jane%20Doe').send({ roll: 0 }))
-      .then(() => chai.request(server).put('/api/Jane%20Doe').send({ roll: 0 }))
-      .then(() => chai.request(server).put('/api/Jane%20Doe').send({ roll: 0 }))
-      .then(() => chai.request(server).put('/api/Jane%20Doe').send({ roll: 0 }))
-      .then(() => chai.request(server).put('/api/Jane%20Doe').send({ roll: 0 }))
-      .then(() => chai.request(server).put('/api/Jane%20Doe').send({ roll: 0 }))
-      .then(res => {
+  it('should have score 0 if all updates are 0', done => {
+    chai.request(server)
+      .put('/api/Jane%20Doe')
+      .send({ roll: 0 })
+      .end((err, res) => {
         res.should.have.status(200);
         res.body.should.be.a('object');
         res.body.should.have.property('score').eql(0);
-        res.body.should.have.property('frames').with.length(10);
-        res.body.should.have.property('onFrame').eql(10);
-        res.body.should.have.property('gameOver').eql(true);
-      })
-      .catch(err => { throw err })
-      .then(() => chai.request(server).delete('/api/Jane%20Doe'));
-  });
-
-  it('should have score 300 if all rolls are 10', () => {
-    chai.request(server).put('/api/Jane%20Doe').send({ roll: 10 })
-      .then(() => chai.request(server).put('/api/Jane%20Doe').send({ roll: 10 }))
-      .then(() => chai.request(server).put('/api/Jane%20Doe').send({ roll: 10 }))
-      .then(() => chai.request(server).put('/api/Jane%20Doe').send({ roll: 10 }))
-      .then(() => chai.request(server).put('/api/Jane%20Doe').send({ roll: 10 }))
-      .then(() => chai.request(server).put('/api/Jane%20Doe').send({ roll: 10 }))
-      .then(() => chai.request(server).put('/api/Jane%20Doe').send({ roll: 10 }))
-      .then(() => chai.request(server).put('/api/Jane%20Doe').send({ roll: 10 }))
-      .then(() => chai.request(server).put('/api/Jane%20Doe').send({ roll: 10 }))
-      .then(() => chai.request(server).put('/api/Jane%20Doe').send({ roll: 10 }))
-      .then(() => chai.request(server).put('/api/Jane%20Doe').send({ roll: 10 }))
-      .then(() => chai.request(server).put('/api/Jane%20Doe').send({ roll: 10 }))
-      .then(res => {
-        res.should.have.status(200);
-        res.body.should.be.a('object');
-        res.body.should.have.property('score').eql(10);
         res.body.should.have.property('frames').with.length(1);
-        res.body.should.have.property('onFrame').eql(2);
+        res.body.should.have.property('onFrame').eql(1);
         res.body.should.have.property('gameOver').eql(false);
-      })
-      .catch(err => { throw err });;
+        done();
+      });
   });
-});
-
-process.on('unhandledRejection', (reason) => {
-  console.error(reason);
-});
+})
