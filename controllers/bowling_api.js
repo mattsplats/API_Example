95f83d7825c68d83e1b8731d4a5dd8efdf862992
Matt Davis <matt_davis@me.com>
2017-04-12 19:27:55 -0500

    db, controller, setup ready for test

--- controllers/bowling_api.js
+++ controllers/bowling_api.js
@@ -2,7 +2,8 @@
 
 const express = require('express'),
       router  = express.Router(),
-      db      = require('../db/mock_db.js');
+      db      = require('../db/mock_db.js'),
+      bowl    = require('../helpers/bowling.js');
 
 
 /**
@@ -59,16 +60,23 @@ router.get('/:name', (req, res) => {
  */
 router.put('/:name', (req, res) => {
   const name     = req.params.name,
-        lastRoll = req.body.lastRoll;
+        currRoll = req.body.currRoll;
 
   if (typeof name === 'undefined' || name === '') {
     res.status(400).send('Name parameter required');
 
-  } else if (typeof lastRoll !== 'number' || lastRoll < 0 || lastRoll > 10) {
+  } else if (typeof currRoll !== 'number' || currRoll < 0 || currRoll > 10) {
     res.status(400).send('Last roll must be a number between 0 and 10');
 
   } else {
-    res.json(db.update(name, lastRoll));
+    const player = db.get(name);
+
+    if (bowl.validateRoll(player, currRoll))
+
+    // Not required here since we're mutating the object, but would be in a normal db
+    // db.set(name, player);
+
+    res.status(400).send('Name parameter required');
   }
 });
 
--- helpers/bowling.js
+++ helpers/bowling.js
@@ -2,27 +2,15 @@
 
 module.exports = {
   /**
-   * Gets the current and previous two frames
-   * @param p - player object
-   * @returns {Object} - contains current and previous two frames
-   */
-  getRecentFrames: function (p) {
-    const currFrame  = p.frames[p.onFrame - 1],
-          prevFrame1 = p.onFrame > 1 ? p.frames[p.onFrame - 2] : undefined,
-          prevFrame2 = p.onFrame > 2 ? p.frames[p.onFrame - 3] : undefined;
-
-    return {currFrame, prevFrame1, prevFrame2};
-  },
-
-  /**
    * Updates player score and scores across frames as needed
-   * @param p - player object
-   * @param currRoll - value of current roll (where 0 <= roll <= 10)
+   * @param {Object} p - player object
+   * @param {Object} currFrame - current frame object
+   * @param {Object} prevFrame1 - prev frame object
+   * @param {Object} prevFrame2 - prev prev frame object
+   * @param {number} currRoll - value of current roll (where 0 <= roll <= 10)
    */
-  updateScores: function (p, currRoll) {
-    const {currFrame, prevFrame1, prevFrame2} = this.getRecentFrames(p);
+  updateScores: function (p, currFrame, prevFrame1, prevFrame2, currRoll) {
 
-      // Update frame scores and player score
       // If both previous frames were strikes, add current roll's score to current and both previous frames
       if (prevFrame1 && prevFrame2 && prevFrame1.roll1 === 'X' && prevFrame2.roll1 === 'X') {
         currFrame.score  += currRoll * 3;
@@ -43,21 +31,25 @@ module.exports = {
 
   /**
    * Updates frames and player score
-   * @param p - player object
-   * @param currRoll - value of current roll (where 0 <= roll <= 10)
-   * @returns p - mutated player object
+   * @param {Object} p - player object
+   * @param {number} currRoll - value of current roll (where 0 <= roll <= 10)
+   * @returns {Object} - mutated player object (currently unused)
    */
   updatePlayer: function (p, currRoll) {
     if (p.gameOver) return p;
     
     if (p.frames.length === p.onFrame) p.frames.push({score: p.score});  // Create current frame object if missing
     
+    const currFrame  = p.frames[p.onFrame - 1],
+          prevFrame1 = p.onFrame > 1 ? p.frames[p.onFrame - 2] : undefined,
+          prevFrame2 = p.onFrame > 2 ? p.frames[p.onFrame - 3] : undefined;
+
     // If on frame ten, handle in frameTenUpdate
     if (p.onFrame === 10) {
-      return this.frameTenUpdate(p, currRoll);
+      return this.frameTenUpdate(p, currFrame, prevFrame1, prevFrame2, currRoll);
 
     } else {
-      this.updateScores(p, currRoll);
+      this.updateScores(p, currFrame, prevFrame1, prevFrame2, currRoll);
 
       const currFrame = p.frames[p.onFrame - 1];
 
@@ -85,11 +77,14 @@ module.exports = {
 
   /**
    * Special player update logic for dealing with the tenth frame
-   * @param p - player object
-   * @param currRoll - value of current roll (where 0 <= roll <= 10)
-   * @returns p - mutated player object
+   * @param {Object} p - player object
+   * @param {Object} currFrame - current frame object
+   * @param {Object} prevFrame1 - prev frame object
+   * @param {Object} prevFrame2 - prev prev frame object
+   * @param {number} currRoll - value of current roll (where 0 <= roll <= 10)
+   * @returns {Object} - mutated player object (currently unused)
    */
-  frameTenUpdate: function (p, currRoll) {
+  frameTenUpdate: function (p, currFrame, prevFrame1, prevFrame2, currRoll) {
     const {currFrame, prevFrame1, prevFrame2} = this.getRecentFrames(p);
 
     // On third roll
@@ -109,7 +104,7 @@ module.exports = {
       currFrame.score += currRoll;
       p.score         += currRoll;
 
-      // The game MUST be over
+      // The game MUST be over!
       p.gameOver = true;
     
 
@@ -144,16 +139,16 @@ module.exports = {
 
   /**
    * Validates roll value input
-   * @param p - player object
-   * @param currRoll - value of current roll (where 0 <= roll <= 10)
+   * @param {Object} p - player object
+   * @param {number} currRoll - value of current roll (where 0 <= roll <= 10)
    * @returns {boolean} - true if valid, otherwise false
    */
   validateRoll: function (p, currRoll) {
     if (currRoll < 0 && currRoll > 10) return false;
     
-    const {currFrame, prevFrame1, prevFrame2} = this.getRecentFrames(p);
-    
     // The remaining tests are for attempts to roll a combined value > 10 on a single rack of pins.
+    const currFrame = p.frames[p.onFrame - 1];
+
     // Frames 1 - 9
     if (p.onFrame < 10) {
       if (typeof currFrame.roll1 !== 'undefined' && currFrame.roll1 + currRoll > 10) return false;
